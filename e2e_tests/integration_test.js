c76271221fe83172f2bc61c026d0df67463bf0e9
Joe Heck <heckj@mac.com>
2018-03-11 10:09:11 -0700

    testing

--- e2e_tests/integration_test.js
+++ e2e_tests/integration_test.js
@@ -1,27 +1,87 @@
-var assert = require('assert');
+const request = require('supertest');
+const k8s = require('@kubernetes/client-node');
+const util = require('util');
+const path = require('path');
+const _ = require('lodash');
 
-describe('integration Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
+var chai = require('chai')
+  , expect = chai.expect
+  , should = chai.should();
+
+var k8sApi = k8s.Config.defaultClient();
 
 describe('kubernetes', function() {
-  it('should list pods', function() {
-    const k8s = require('@kubernetes/client-node');
 
-    var k8sApi = k8s.Config.defaultClient();
-    var util = require('util');
-    // console.log(util.inspect(k8sApi));
-    return k8sApi.listNamespacedPod('default')
-    .then((res) => {
-      // console.log(util.inspect(res));
-      console.log(res.body);
-    },
-    (err) => {
-      console.log('Error!: ' + err);
-    });
-  })
-})
+  describe('cluster', function() {
+
+    it('should have a healthy cluster', function() {
+      return k8sApi.listComponentStatus()
+      .then((res) => {
+        // console.log(util.inspect(res.body));
+        res.body.items.forEach(function(component) {
+          // console.log(util.inspect(value));
+          expect(component.conditions[0].type).to.equal("Healthy");
+          expect(component.conditions[0].status).to.equal("True");
+        })
+      }, (err) => {
+        expect(err).to.be.null;
+      });
+    })
+
+    it('should deploy the manifests', function() {
+      var manifest_directory = path.normalize(path.join(path.dirname(__filename), '..', '/deploy'))
+
+      const exec = util.promisify(require('child_process').exec);
+      return exec('kubectl apply -f '+manifest_directory)
+      .then((res) => {
+        // console.log(util.inspect(res));
+        expect(res.stdout).to.not.be.null;
+        expect(res.stderr).to.be.empty;
+      }, (err) => {
+        expect(err).to.be.null;
+      })
+    })
+
+    describe('should repeat until the pods are ready', function() {
+      // Mocha supports a retry mechanism limited by number of retries...
+      this.retries(30);
+      // an a default timeout of 20,000ms that we can increase
+      this.timeout(300000);
+
+      it('check to see that all pods are reporting ready', function() {
+        return new Promise(function(resolve, reject) {
+          console.log('delay 5 seconds...')
+          setTimeout(() => resolve(1), 5000);            
+        }).then(function(result) {
+          return  k8sApi.listNamespacedPod('default')
+          .then((res) => {
+            res.body.items.forEach(function(pod) {
+              var readyCondition = _.filter(pod.status.conditions, { 'type': 'Ready' })
+              //console.log("checking: "+pod.metadata.name+" ready: "+readyCondition[0].status);
+              expect(readyCondition[0].status).to.equal('True')
+            }) // pod forEach
+          })
+        })
+      }) // it
+
+      // it.skip('should access by pod...', function() {
+      //   return k8sApi.proxyGETNamespacedPodWithPath("nodejs-0", "default", "/")
+      //   .then(function(res) {
+      //     // console.log(util.inspect(res));
+      //     console.log(res.body);
+      //   });
+      // })
+  
+    }); // describe - pods available
+
+    it('should access by service name', function() {
+      return k8sApi.proxyGETNamespacedPod("nodejs-0", "default")
+      .then(function(res) {
+        console.log("yep")
+      })
+    })
+
+    
+  }); //describe - cluster
+
+}); // describe - kubernetes
